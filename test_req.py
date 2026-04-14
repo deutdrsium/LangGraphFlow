8690fe3450e3b7d0e83ff925381ea3bdd2b9ae26
System Administrator <root@feikaixuandeMacBook-Air.local>
2026-04-14 08:35:21 +0800

    添加了其他方向的支持

--- test_req.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-import json
-try:
-    req = urllib.request.Request("http://127.0.0.1:8001/api/active_sessions")
-    response = urllib.request.urlopen(req)
-    print(response.read().decode())
-except Exception as e:
-    print(e)
